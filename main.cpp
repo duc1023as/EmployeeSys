b7bde8036685e8a2d76465a0237f0805cc8b0826
DUC DO <duc1023as@gmail.com>
2024-01-16 23:44:52 +0700

    change comment

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show(); //test commit ....
+    w.show(); //test commit .... ...
     return a.exec();
 }
