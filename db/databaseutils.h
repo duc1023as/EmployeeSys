26c027b0f3000557d931a86e1ac2b1ca69b6a328
DUC DO <duc1023as@gmail.com>
2024-01-25 23:15:53 +0700

    add img folder

--- db/databaseutils.h
+++ db/databaseutils.h
@@ -3,21 +3,34 @@
 
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QSqlQueryModel>
+#include "component/querymodel.h"
 #include <QSqlError>
 #include <QDebug>
 #include <QCryptographicHash>
+#include <QTableView>
+#include <QMutex>
 
 class DatabaseUtils
 {
 public:
-    DatabaseUtils();
     ~DatabaseUtils();
     bool connectToDB(const QString& dbName);
     QString hashPassword(const QString& password);
     bool checkValidUser(const QString& username,const QString& password);
     QSqlDatabase& getInfoDB() const {return *db;}
+    QString getTotalEmployee();
+    QString getTotalDepartment();
+    QString getTotalJob();
+    void setEmployeeDetails(QTableView *table);
+    void searchEmployeeDetails(QTableView *table,const QString& searchText);
+    static DatabaseUtils* getInstance();
+
 private:
     QSqlDatabase *db;
+    static DatabaseUtils* instance;
+    static QMutex locker;
+    DatabaseUtils();
 };
 
 #endif // DATABASEUTILS_H
